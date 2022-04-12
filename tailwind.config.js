bc29e019b022c31c78a6a8f5afb8583527f445d4
sboursen <dev.sboursen@gmail.com>
2022-04-12 14:55:45 +0100

    Define project template media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      xs: '320px',
+      sm: '480px',
+      md: '640px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   plugins: [],
 };
