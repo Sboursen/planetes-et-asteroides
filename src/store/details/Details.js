4809b77fee7d5a63974890e0c08fbd5151f1219d
sboursen <dev.sboursen@gmail.com>
2022-04-12 13:33:08 +0100

    Add getBody action creators and reducer

---
+++ src/store/details/Details.js
@@ -0,0 +1,68 @@
+import { getBodyById } from '../api/solarSystemApi';
+
+const GET_BODY_REQUEST = 'PLANETES_ET_ASTEROIDES/BODY/GET_REQUEST';
+const GET_BODY_SUCCESS = 'PLANETES_ET_ASTEROIDES/BODY/GET_SUCCESS';
+const GET_BODY_FAILURE = 'PLANETES_ET_ASTEROIDES/BODY/GET_FAILURE';
+
+const initialState = {
+  loading: false,
+  body: {},
+  error: '',
+};
+
+export function getBodyRequest() {
+  return {
+    type: GET_BODY_REQUEST,
+  };
+}
+
+export function getBodySuccess(body) {
+  return {
+    type: GET_BODY_SUCCESS,
+    payload: body,
+  };
+}
+
+export function getBodyFailure(error) {
+  return {
+    type: GET_BODY_FAILURE,
+    payload: error,
+  };
+}
+
+export function getBody(id) {
+  return (dispatch) => {
+    getBodyRequest();
+    getBodyById(id)
+      .then((body) => {
+        dispatch(getBodySuccess(body));
+      })
+      .catch((error) => {
+        dispatch(getBodyFailure(error));
+      });
+  };
+}
+
+export default function getBodyReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_BODY_REQUEST:
+      return { ...state, loading: true };
+
+    case GET_BODY_SUCCESS:
+      return {
+        loading: false,
+        body: action.payload,
+        error: '',
+      };
+
+    case GET_BODY_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
