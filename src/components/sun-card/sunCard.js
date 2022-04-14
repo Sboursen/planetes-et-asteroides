8c98fbd38011ef208648287c4bd7dd0b8ef2fc4e
sboursen <dev.sboursen@gmail.com>
2022-04-14 17:53:22 +0100

    Enhance the sun card

--- src/components/sun-card/sunCard.js
+++ src/components/sun-card/sunCard.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function SunCard() {
+  const bodies = useSelector((state) => state.bodies.bodiesList);
   const massValue = 1.989;
   const massExponent = 30;
   const volValue = 1.409;
   const volExponent = 18;
 
+  const groupByType = (bodies) => {
+    const types = new Map();
+    bodies.forEach((body) => {
+      const type = body.bodyType;
+      if (types.has(type)) {
+        types.set(type, types.get(type) + 1);
+      } else {
+        types.set(type, 1);
+      }
+    });
+    return Array.from(types);
+  };
+
   return (
-    <div className="flex flex-row justify-around items-start shadow-sm rounded">
-      <div className="flex flex-col w-fit items-end">
+    <div className="flex flex-row justify-around items-start shadow-sm rounded py-2">
+      <div className="flex flex-col w-fit items-start">
         <h2 className="text-5xl font-control whitespace-nowrap">The Sun</h2>
-        <h3 className="text-xl">8 planets</h3>
+        <ul className="flex flex-col pl-4">
+          {groupByType(bodies).map((element, index) => {
+            if (element[1] >= 2) {
+              return (
+                // eslint-disable-next-line react/no-array-index-key
+                <li key={index} className="w-fit">
+                  {`${element[0]} ${element[1]}`}
+                </li>
+              );
+            }
+            return <></>;
+          })}
+        </ul>
       </div>
-      <ul className="border-2 border-white bg-grid-one rounded ">
+      <ul className="border-2 border-white bg-grid-one rounded p-3">
         <li>Type: Star</li>
         <li className="block">
           <span className="inline">Mass: </span>
@@ -30,7 +57,6 @@ export default function SunCard() {
             {' '}
             x 10
             <sup>{volExponent}</sup>
-            {' '}
             km
             <sup>3</sup>
           </pre>
