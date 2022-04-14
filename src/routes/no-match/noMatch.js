18a555687cc9d1de3a802a50a68e21e74ba2052c
sboursen <dev.sboursen@gmail.com>
2022-04-14 20:04:14 +0100

    Style the NoMatch route

--- src/routes/no-match/noMatch.js
+++ src/routes/no-match/noMatch.js
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
-    <section className="mt-6 px-12 overflow-auto rounded w-full">
-      <h1 className="text-3xl">404 not found</h1>
+    <section className="mt-6 flex flex-col items-center gap-6 rounded w-full">
+      <div className="flex flex-col items-center">
+        <h1 className="text-3xl">404 not found</h1>
+        <iframe
+          title="lost in space"
+          src="https://giphy.com/embed/3o7btZ3T6y3JTmjg4w"
+          width="480"
+          height="270"
+          frameBorder="0"
+          loading="lazy"
+          allowFullScreen
+        />
+        <p>
+          <a
+            className=" self-stretch"
+            href="https://giphy.com/gifs/epitaphrecords-music-video-3o7btZ3T6y3JTmjg4w"
+          >
+            via GIPHY
+          </a>
+        </p>
+      </div>
+      <Link className="text-3xl underline hover:text-red-500" to="/">
+        GO BACk TO HOME
+      </Link>
     </section>
   );
 }
