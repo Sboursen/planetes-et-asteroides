246b0bfae098199661c4b852ee27f476c078ae7a
sboursen <dev.sboursen@gmail.com>
2022-04-14 19:41:55 +0100

    Redirect to noMatch route on wrong body id in details

--- src/routes/planet-details/planetDetails.js
+++ src/routes/planet-details/planetDetails.js
@@ -1,16 +1,18 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import TitleCard from '../../components/title-card/titleCard';
 import { getBody } from '../../store/details/Details';
 import BodiesList from '../../components/bodies-list/bodiesList';
 
 export default function PlanetDetails() {
   const params = useParams();
+  const navigate = useNavigate();
   const bodies = useSelector((state) => state.bodies.bodiesList);
-  const { bodyId } = params;
+  const error = useSelector((state) => state.body.error);
   const bodyDetails = useSelector((state) => state.body.body);
   const dispatch = useDispatch();
+  const { bodyId } = params;
   useEffect(() => {
     dispatch(getBody(bodyId));
   }, [bodyId]);
@@ -33,6 +35,9 @@ export default function PlanetDetails() {
     return <></>;
   };
 
+  if (error !== '') {
+    navigate('/*');
+  }
   return (
     <section className="py-4 rounded w-full">
       <TitleCard name={name} type={type} mass={mass} vol={vol} />
