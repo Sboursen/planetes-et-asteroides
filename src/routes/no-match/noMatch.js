c29ac826116ec47d2a78604b4e07b6b687f49f4f
sboursen <dev.sboursen@gmail.com>
2022-04-15 08:44:25 +0100

    Update the readme

--- src/routes/no-match/noMatch.js
+++ src/routes/no-match/noMatch.js
@@ -15,14 +15,6 @@ export default function Home() {
           loading="lazy"
           allowFullScreen
         />
-        <p>
-          <a
-            className=" self-stretch"
-            href="https://giphy.com/gifs/epitaphrecords-music-video-3o7btZ3T6y3JTmjg4w"
-          >
-            via GIPHY
-          </a>
-        </p>
       </div>
       <Link className="text-3xl underline hover:text-red-500" to="/">
         GO BACk TO HOME
