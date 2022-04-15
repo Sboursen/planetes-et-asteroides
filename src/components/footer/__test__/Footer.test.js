8deae9ff03ee494c3db1ff756572b0f8c1e2e2c4
sboursen <dev.sboursen@gmail.com>
2022-04-15 10:50:46 +0100

    Test the Footer Component

---
+++ src/components/footer/__test__/Footer.test.js
@@ -0,0 +1,23 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Footer from '../Footer';
+
+describe('Footer', () => {
+  const handleBodyCardClick = (id) => id;
+  afterEach(cleanup);
+
+  it('renders correctly in the DOM', () => {
+    const tree = renderer.create(<Footer />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('contains the link to the license', () => {
+    render(<Footer />);
+
+    const nameElement = screen.getByText('MIT license');
+    const { classList } = nameElement;
+
+    expect(classList).toContain('hover:text-red-500');
+  });
+});
